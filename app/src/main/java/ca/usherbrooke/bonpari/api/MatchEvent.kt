68fbc12b078aa0e380d4c7840c43e4ce8781e746
Quentin RA <quentin.ramsamy.cf@gmail.com>
2022-10-22 13:31:01 -0400

    fixing bug

--- app/src/main/java/ca/usherbrooke/bonpari/api/MatchEvent.kt
+++ app/src/main/java/ca/usherbrooke/bonpari/api/MatchEvent.kt
@@ -3,8 +3,9 @@ package ca.usherbrooke.bonpari.api
 import com.squareup.moshi.Json
 import java.io.Serializable
 
+// todo: not a problem in the code, but "-0" is "0"
 data class MatchEvent(@Json(name = "type") val type : Int,
-                      @Json(name = "result") val result : Int) : Serializable {
+                      @Json(name = "result") val result : String) : Serializable {
 
     fun isContestation() = type == 1
     fun isPointScored() = type == 2
@@ -15,17 +16,18 @@ data class MatchEvent(@Json(name = "type") val type : Int,
     val contestation : ContestationData?
         get() {
             return if (isContestation())
-                if (result < 0)
-                    ContestationData(result == -Match.PlayerIndex.Player1.index, false)
+                if (result.startsWith("-"))
+                    ContestationData(result.toInt() == Match.PlayerIndex.Player1.index, false)
                 else
-                    ContestationData(result == Match.PlayerIndex.Player1.index, true)
+                    ContestationData(result.toInt() == Match.PlayerIndex.Player1.index, true)
             else
                 null
         }
+
     val point : PointData?
         get() {
             return if (isPointScored())
-                PointData(result == Match.PlayerIndex.Player1.index)
+                PointData(result.toInt() == Match.PlayerIndex.Player1.index)
             else
                 null
         }
