5333556e2242d466b5e278fab2392a588c61a732
Quentin RA <quentin.ramsamy.cf@gmail.com>
2022-11-02 19:09:17 -0400

    remove ss

--- app/src/main/java/ca/usherbrooke/bonpari/view/Utils.kt
+++ app/src/main/java/ca/usherbrooke/bonpari/view/Utils.kt
@@ -1,7 +1,6 @@
 package ca.usherbrooke.bonpari.view
 
 import android.content.Context
-import android.util.Log
 import ca.usherbrooke.bonpari.R
 import ca.usherbrooke.bonpari.api.Match
 import ca.usherbrooke.bonpari.api.MatchEvent
@@ -9,13 +8,9 @@ import ca.usherbrooke.bonpari.api.MatchEvent
 fun formatSecondToHoursMinutes(seconds: Int): String {
     var h = (seconds / 3600).toString()
     var m = ((seconds / 60) % 60).toString()
-    var s = (seconds % 60).toString()
     if (h.length == 1) h = "0$h"
     if (m.length == 1) m = "0$m"
-    if (s.length == 1) s = "0$s"
-    val r = "$h:$m:$s"
-    Log.d("CAL", "Show $r for $seconds")
-    return r
+    return "$h:$m"
 }
 
 fun MatchEvent.formatToString (match: Match, context: Context) : String {
